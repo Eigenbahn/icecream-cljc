dc3f5f8deefc0d0128b172ceafb91c2cf920b38f
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-04-23 09:38:45 +0200

    Added project file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject eigenbahn/icecream "0.1.0"
+  :description "Port of icrecream to Clojure(Script)."
+  :url "https://github.com/Eigenbahn/icecream-cljc"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
